73095f33a4803e34ccd7769e45bd9b276aaddbf3
RashedMigdady <migdadyrashed@gmail.com>
2020-01-18 03:42:52 +0200

    task

--- company/src/company/Company.java
+++ company/src/company/Company.java
@@ -2,21 +2,15 @@ package company;
 
 public class Company {
     
-    public static int recursion(int x){
-        if (x == 30){
-          return 0;
-        }
-            
-        return recursion(x+1);
-    }
-
-    
+   
     public static void main(String[] args) {
        
-       Contractor obj1 = new Contractor("rashed" ,10 , 3);
+       Contractor obj1 = new Contractor("rashed" ,10 , 3);     //The object it is not Primative because of is a reference type store handles to object in heap 
        FullTimeEmployee obj2 = new FullTimeEmployee("ALi" , 5 );
+       
        int rashed = obj1.calculateSalary();
        int ali = obj2.calculateSalary();
+       
        System.out.println(rashed);
        System.out.println(ali);
        
